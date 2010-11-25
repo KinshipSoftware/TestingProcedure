370f24786cfeda20c5206d463edd001ab5b2f37f
Vlado Plaga <vlado.plaga@mpi.nl>
2010-11-25 11:02:08 +0000

    Some new test, initial script (with some error)

---
+++ txt2xsd.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Bash script to generate a Arbil test XSD (XML) file from a plain text testcases file
+# ignores comments and whitespace in the testcases file
+#
+# plain text file format:
+#
+# NodeSearchTest
+#
+#   OpenSearchWindow "From the local corpus tree right click and select search; a search window should be shown."
+#   LocalSearch      "From the local corpus tree right click and select search; enter [...]"
+#
+# Nodes are one word per line
+# Tests are one word plus an expression in "quotation marks"
+#
+# Version 0.1 / Vlado Plaga 2010-11-25 / not working yet
+
+testcasesfile="testcases.txt"
+
+fileheader='<?xml version="1.0" encoding="utf-8"?>
+
+<xs:schema elementFormDefault="qualified" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:ann="http://www.clarin.eu">
+
+    <xs:element name="ArbilBugCheck">
+
+        <xs:complexType>
+
+            <xs:sequence>
+
+                <xs:element name="DateTime" type="xs:string" maxOccurs="1" minOccurs="1" ann:documentation="The date time the test was started" ann:displaypriority="1"/>
+
+                <xs:element name="ArbilVersion" type="xs:string" maxOccurs="1" minOccurs="1" ann:documentation="The version of Arbil being tested" />
+
+                <xs:element name="OSVersion" type="xs:string" maxOccurs="1" minOccurs="1" ann:documentation="The operating system type and version being tested on" />
+
+                <xs:element name="Type" type="TestResult" />
+';
+
+testfooter='                            <xs:element name="ErrorDescription" maxOccurs="unbounded" minOccurs="1" type="xs:string" />
+
+                        </xs:sequence>
+
+                    </xs:complexType>
+
+                </xs:element>
+';
+
+testresults='    <xs:simpleType name="TestResult">
+
+        <xs:restriction base="xs:string">
+
+            <xs:enumeration value="Works" />
+
+            <xs:enumeration value="Mostly" />
+            
+            <xs:enumeration value="Strange" />
+            
+            <xs:enumeration value="Fails" />
+
+        </xs:restriction>
+
+    </xs:simpleType>
+';
+
+
+testheader='                    <xs:complexType>
+
+                        <xs:sequence>';
+
+# boolean variable to make sure tags are closed with $testfooter
+insidetest=false;
+
+echo "$fileheader";
+
+while read line; do
+  # ignore empty and comment lines
+  if [[ $line != "" && ! $(echo $line | grep " *#" ) ]]; then 
+    if [[ $line == *\ * ]]; then
+      insidetest=0; # "0" is "true" for bash
+      element="$(echo $line | sed s/\ .*//)";
+      annotation="$(echo $line | sed s/$element//)";
+      echo "                            <xs:element name=\"$element\"  maxOccurs=\"1\" minOccurs=\"1\" ann:documentation=\"$annotation\" type=\"TestResult\" />";
+      echo;
+    else
+      if [ $insidetest ]; then
+        echo "$testfooter";
+      fi;
+      insidetest=false;
+      echo "                <xs:element name=\"$line\" minOccurs=\"1\" maxOccurs=\"unbounded\">";
+      echo;
+      echo "$testheader";
+    fi
+  fi
+done < $testcasesfile
+
+echo "$testfooter";
+
+echo "$testresults";
+
+echo "</xs:schema>";
