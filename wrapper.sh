23eb4b294d6fb693a54d19fc0f2b75a8eaa841d0
Alexander Koenig <Alexander.Koenig@mpi.nl>
2011-02-03 07:59:23 +0000

    vlapla fixed Lamustestcases and improved wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-
-num=0
 input='testcases.txt'
 output='TestingCheck4.xsd'
 
-for i in `find -iname "*testcases.txt" `
+for file in *$input
 do
-   :
-   newArray[$num]=`echo $i | perl -nle 'm/\.\/(.+)testcases\.txt/; print $1'`
-   num=$(($num + 1))
+ xsdfile=$(echo $file | sed "s/$input/$output/")
+ echo "creating $xsdfile from $file..."
+ ./txt2xsd.sh $file > $xsdfile
 done
-
-
-for i in "${newArray[@]}"
-do
-
-        echo $i$input
-        echo $i$output
-       result=`./txt2xsd.sh $i$input`
-	`echo $result > $i$output`
-done
-
-
